0115e2f455f4e38762255ecc6e966fdf9171d9d9
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-12-07 12:36:28 +0000

    removing corsheader from settings

--- gymbud/gymbud/settings.py
+++ gymbud/gymbud/settings.py
@@ -44,11 +44,9 @@ INSTALLED_APPS = [
     'shouts',
     'likes',
     'inbox',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
